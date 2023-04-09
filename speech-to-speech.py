7470bc4192ee2e60c32b23a8ef6c32119e814ec0
Liam Dugan <ldugan@seas.upenn.edu>
2023-04-09 18:23:21 -0400

    Updated pipeline to use segments

--- speech-to-speech.py
+++ speech-to-speech.py
@@ -4,90 +4,66 @@ import speech_recognition as sr
 from tempfile import NamedTemporaryFile
 from datetime import datetime, timedelta
 from time import sleep
-from fuzzywuzzy import process
 import pyaudio
 from playsound import playsound
-import sounddevice as sd
-from pydub import AudioSegment
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--model", default="medium", help="Model to use",
-                    choices=["tiny", "base", "small", "medium", "large"])
-parser.add_argument("--energy_threshold", default=600,
-                    help="Energy level for mic to detect.", type=int)
-parser.add_argument("--record_timeout", default=3,
-                    help="How real time the recording is in seconds.", type=float)
-parser.add_argument("--phrase_timeout", default=15,
-                    help="How much empty space between recordings before we "
-                            "consider it a new line in the transcription.", type=float)  
-parser.add_argument("--microphone_id", default=1,
-                    help="ID for the input microphone", type=int)
+parser.add_argument("--model", default="medium", help="Model to use", choices=["tiny", "base", "small", "medium", "large"])
+parser.add_argument("--energy_threshold", default=600, help="Energy level for mic to detect.", type=int)
+parser.add_argument("--record_timeout", default=0.5, help="How often we run audio through whisper", type=float)
+parser.add_argument("--phrase_timeout", default=1, help="How much silence before we clear the buffer.", type=float)  
+parser.add_argument("--microphone_id", default=1, help="ID for the input microphone", type=int)
+parser.add_argument("--verbose", action='store_true', help='Whether to print out the intermediate results or not')
 args = parser.parse_args()
-
-def find_longest_common_substring(s1, s2):
-    match = process.extractBests(s1, [s2[:i] for i in range(len(s2))], limit=None)
-    sorted_matches = sorted(match, key=lambda x: (-x[1], -len(x[0])))
-    return sorted_matches[0][0]
 
 #Eleven Labs
 url = "https://api.elevenlabs.io/v1/text-to-speech/TxGEqnHWrfWFTfGW9XjX"
 headers = {
     "xi-api-key": "fff703947a1ec48a8e57abc4507bcb30"
 }
-
-source = sr.Microphone(sample_rate=16000, device_index=args.microphone_id)
-
-def record(q):
-    recorder = sr.Recognizer()
-    recorder.energy_threshold = args.energy_threshold
-    recorder.dynamic_energy_threshold = False
-
-    with source: 
-        recorder.adjust_for_ambient_noise(source)
-
-        while True:
-            try:
-                audio = recorder.listen(source, phrase_time_limit=args.record_timeout)
-                q.put(audio.get_raw_data())
-            except KeyboardInterrupt:
-                q.put(None)
-                break
-
-
+    
 def recognize(q):
     phrase_time = None
-    current_phrase = ''
+    spoken = 0.0
+    tolerance = 1.0
+    prev = None
     phrase_buffer = bytes()
     temp_file = NamedTemporaryFile().name
-    transcription = ['']
     
     # Load / Download model
     model = args.model
     audio_model = whisper.load_model(model)
+    
+    source = sr.Microphone(sample_rate=16000, device_index=args.microphone_id)
 
-    audio_queue = Queue()
+    recorder = sr.Recognizer()
+    recorder.energy_threshold = args.energy_threshold
+    recorder.dynamic_energy_threshold = False
 
-    p = Process(target=record, args=(audio_queue, ))
-    p.start()
+    data_queue = Queue()
+
+    with source: 
+        recorder.adjust_for_ambient_noise(source)
+        
+    def record_callback(_, audio:sr.AudioData) -> None:
+        """
+        Threaded callback function to recieve audio data when recordings finish.
+        audio: An AudioData containing the recorded bytes.
+        """
+        # Grab the raw bytes and push it into the thread safe queue.
+        data = audio.get_raw_data()
+        data_queue.put(data)
+
+    recorder.listen_in_background(source, record_callback, phrase_time_limit=args.record_timeout)
 
     # Cue the user that we're ready to go.
-    print("Model loaded.\n")
-
+    print("Ready:")
     while True:
         try:
-            now = datetime.utcnow()
-            if not audio_queue.empty():
-                # If enough time has passed between recordings, consider the phrase complete.
-                # Clear the current working audio buffer to start over with the new data.
-                if phrase_time and now - phrase_time > timedelta(seconds=args.phrase_timeout):
-                    phrase_buffer = bytes()
-                    current_phrase = ''
-                
-                # This is the last time we received new audio data from the queue.
-                phrase_time = now
+            if not data_queue.empty():
 
-                while not audio_queue.empty():
-                    data = audio_queue.get()
+                while not data_queue.empty():
+                    data = data_queue.get()
                     phrase_buffer += data
 
                 audio_data = sr.AudioData(phrase_buffer, source.SAMPLE_RATE, source.SAMPLE_WIDTH)
@@ -99,32 +75,65 @@ def recognize(q):
 
                 # Read the transcription.
                 result = audio_model.transcribe(temp_file, fp16=torch.cuda.is_available(), task="translate", language="japanese")
-                text = result['text'].strip()
 
-                # If this is a new phrase then we just put the text into the queue
-                if not current_phrase:
-                    q.put(text)
-                else:
-                    # If not, calculate the overlap between tokens of the phrase and the new text
-                    substring = find_longest_common_substring(current_phrase, text)
-                    q.put(text[len(substring):])
+                if args.verbose:
+                    print(f"[[{[s['text'] for s in result['segments'] if s['start'] > spoken - tolerance]}]]")
 
-                current_phrase = text
+                # If there was a previous result, try to find consensus
+                if prev:
+                    for s in result['segments']:
+                        # If the segment is before where we've already spoken, don't bother
+                        if s['start'] <= spoken - tolerance:
+                            continue
+                        
+                        # If the segment is first and no speech is high, don't speak it
+                        if s['start'] == 0.0 and s['no_speech_prob'] > 0.7:
+                            continue
+                        
+                        # If the segment is past the prev we're done
+                        if s['id'] >= len(prev['segments']):
+                            break
+                        
+                        # Check if there is consensus and if so, speak
+                        if s['text'] == prev['segments'][s['id']]['text']:
+                            q.put(s['text'].strip())
+                            spoken = s['end']
+                        else:
+                            break
 
+                # Save the current result to prev
+                prev = result
+                
+                # This is the last time we received new audio data from the queue.
+                phrase_time = datetime.utcnow()
+                
+            elif phrase_time and datetime.utcnow() - phrase_time > timedelta(seconds=args.phrase_timeout):
+                # if enough time has elapsed, speak all segments currently in the buffer
+                if prev:
+                    for s in prev['segments']:
+                        if s['start'] > spoken - tolerance:
+                            if s['start'] == 0.0 and s['no_speech_prob'] > 0.5:
+                                continue
+                            else:
+                                q.put(s['text'].strip())
+                prev = None
+                spoken = 0.0
+                phrase_buffer = bytes()
+                phrase_time = None
         except KeyboardInterrupt:
-            q.put(None)
             break
 
-if __name__ == '__main__':
+def main():
     q = Queue()
     p = Process(target=recognize, args=(q,))
+    p.daemon = True
     p.start()
 
     while True:
         try:
             data = q.get()
 
-            if data is None: break
+            if data is None: continue
 
             print(data)
 
@@ -146,5 +155,7 @@ if __name__ == '__main__':
             else:
                 print("Error")
         except KeyboardInterrupt:
-            p.join()
             break
+    
+if __name__ == "__main__":
+    main()
